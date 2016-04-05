f7a40edbd395048e9aeedf21b038f8d64aea3e2b
Arshad Chummun <arshad@chummun.com>
2016-04-06 01:36:50 +0400

    Check for current window when getting active tab

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,14 +6,19 @@ chrome.runtime.onConnect.addListener(function (port) {
   // Listen for message from devtools.js
   port.onMessage.addListener(function (message) {
     if (message.sender == 'drupal-template-helper-devtools' && message.message == 'current-tab-url') {
-      // Get the current active tab.
-      chrome.tabs.query({ 'active': true }, function (tabs) {
+      // Get the current window.
+      chrome.windows.getCurrent(function (window) {
+        if (window) {
+          // Get the current active tab.
+          chrome.tabs.query({ 'active': true, 'windowId': window.id }, function (tabs) {
 
-        // Send the url of the active tab to devtools.js
-        if (tabs.length) {
-          port.postMessage({
-            'sender': 'drupal-template-helper-background',
-            'url': tabs[0].url
+            // Send the url of the active tab to devtools.js
+            if (tabs.length) {
+              port.postMessage({
+                'sender': 'drupal-template-helper-background',
+                'url': tabs[0].url
+              });
+            }
           });
         }
       });
