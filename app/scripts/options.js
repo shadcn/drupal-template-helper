2bd026fee76689d94a879ac6be499f576bbe1bb5
Arshad Chummun <arshad@chummun.com>
2016-04-13 21:40:00 +0400

    Issue #4: add option to toggle the dark theme

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -5,10 +5,11 @@
 // Attach restore event to dom loaded.
 
 document.addEventListener('DOMContentLoaded', function () {
-  DrupalTemplateHelperOptions.get(['enabledSites', 'showComments'], function (settings) {
+  DrupalTemplateHelperOptions.get(['enabledSites', 'showComments', 'darkTheme'], function (settings) {
     console.log(settings);
     document.getElementById('template-helper-enabled-sites').value = settings.enabledSites || '';
     document.getElementById('template-helper-show-comments').checked = settings.showComments || false;
+    document.getElementById('template-helper-dark-theme').checked = settings.darkTheme || false;
   });
 });
 
@@ -17,7 +18,8 @@ document.getElementById('save').addEventListener('click', function () {
   // Save settings.
   DrupalTemplateHelperOptions.save({
     enabledSites: document.getElementById('template-helper-enabled-sites').value,
-    showComments: document.getElementById('template-helper-show-comments').checked
+    showComments: document.getElementById('template-helper-show-comments').checked,
+    darkTheme: document.getElementById('template-helper-dark-theme').checked
   }, function () {
     DrupalTemplateHelperOptions.alert('Options saved successfully');
   });
--- app/scripts/sidebar.js
+++ app/scripts/sidebar.js
@@ -4,9 +4,15 @@
 // When an element is selected, we figure out its Template Helper comments.
 
 // Show Template templates when Template panel is opened.
-
 window.addEventListener('load', function () {
   DrupalTemplateHelper.showTemplates();
+});
+
+// Add custom class if dark theme is enabled.
+DrupalTemplateHelperOptions.get(['darkTheme'], function (settings) {
+  if (settings.darkTheme) {
+    document.getElementsByTagName('body')[0].className +=' dark-theme';
+  }
 });
 
 // Show Template templates when an element is selected in the Elements panel.
